255762462129b9bb8de5e5b8918f0dc714740086
Riya Mhatre <riyatmhatre@gmail.com>
2026-05-17 15:10:46 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,53 +60,27 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
 
-    implementation(
-        "androidx.lifecycle:lifecycle-runtime-ktx:2.8.3"
-    )
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
 
-    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.activity:activity-compose:1.9.1")
 
-    implementation(
-        "androidx.activity:activity-compose:1.9.0"
-    )
-
-    implementation(
-        platform(
-            "androidx.compose:compose-bom:2024.06.00"
-        )
-    )
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
 
     implementation("androidx.compose.ui:ui")
 
-    implementation(
-        "androidx.compose.ui:ui-graphics"
-    )
+    implementation("androidx.compose.ui:ui-graphics")
 
-    implementation(
-        "androidx.compose.ui:ui-tooling-preview"
-    )
+    implementation("androidx.compose.ui:ui-tooling-preview")
 
-    implementation(
-        "androidx.compose.material3:material3"
-    )
+    implementation("androidx.compose.material3:material3")
 
-    implementation(
-        "androidx.navigation:navigation-compose:2.7.7"
-    )
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
-    implementation(
-        "io.coil-kt:coil-compose:2.6.0"
-    )
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
-    implementation(
-        "androidx.core:core-splashscreen:1.0.1"
-    )
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
-    debugImplementation(
-        "androidx.compose.ui:ui-tooling"
-    )
+    debugImplementation("androidx.compose.ui:ui-tooling")
 
-    debugImplementation(
-        "androidx.compose.ui:ui-test-manifest"
-    )
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
