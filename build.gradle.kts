82b75b54411d2b0bdbfbc3959539e9a5933020db
Riya Mhatre <riyatmhatre@gmail.com>
2026-05-17 09:52:03 +0530

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-// AURA — Top level build file
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-}
